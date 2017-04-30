fd76116ca2890b0d4d82fa4434cc89273d2bed10
Y Chan <ychan@ys-imac.home>
2017-04-30 17:27:53 +0100

    modify photo related classes

--- src/pages/photo/photo.ts
+++ src/pages/photo/photo.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import * as exif from 'exif-js';
 const PLACEHOLDER: string = 'assets/img/photo/placeholder.png';
 /**
  * Generated class for the Photo page.
@@ -16,29 +16,129 @@ const PLACEHOLDER: string = 'assets/img/photo/placeholder.png';
   templateUrl: 'photo.html',
 })
 export class PhotoPage {
-  private base64Image:string = PLACEHOLDER;
-  private placeHolder:string = PLACEHOLDER;
+  private base64Image: string = PLACEHOLDER;
+  private PLACEHOLDER: string = PLACEHOLDER;
+  private coords: Coordinates = null;
+  private locationLoading: boolean = false;
+  private image: Blob = null;
+  
+  
+  constructor(
+    
+    private navCtrl: NavController,
+    private DomSanitizer: DomSanitizer,
+    private platform: Platform,
+    private camera: Camera
+  ) {}
 
-  constructor(private navCtrl: NavController, 
-  private navParams: NavParams,
-  private camera: Camera,
-  private DomSanitizer: DomSanitizer
-  ) {
+  savePicture() {
+    if (this.image) {
+      const metadata = { contentType: (this.image as any).type };
+
+      const filename = `image-${new Date().getTime()}.jpg`;
+      //this.photoStorage.uploadPicture(this.image, this.coords, filename, metadata)
+       // .then(() => this.resetImage())
+       // .catch(() => this.resetImage());
+    }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Photo');
+  resetImage() {
+    this.coords = null;
+    this.image = null;
+    this.base64Image = this.PLACEHOLDER;
   }
 
-   takePicture() {
+  
+
+  takePicture() {
+    if (this.base64Image === this.PLACEHOLDER) {
       this.camera.getPicture({
-      correctOrientation: true,
-      destinationType: this.camera.DestinationType.DATA_URL,
-        }).then((imageData:any) => {
-          this.base64Image = 'data:image/jpeg;base64,' + imageData;
-        }, (err:any) => {
-          console.error(err);
+        correctOrientation: true,
+        targetWidth: 720,
+        destinationType: this.camera.DestinationType.FILE_URI,
+      }).then((fileData) => {
+        this.locationLoading = true;
+        return this.makeBlobFromFile(fileData);
+      }).then((imageData) => {
+        return this.rotateImage(imageData);
+      }).then((rotatedData: Blob) => {
+        this.image = rotatedData;
+        const urlCreator = window.URL || (window as any).webkitURL;
+        this.base64Image = urlCreator.createObjectURL(rotatedData);
+        //this.getGeoCoords().then(coords => this.coords = coords);
       });
-}
+    } else {
+      this.resetCoords();
+      this.base64Image = this.PLACEHOLDER;
+    }
+  }
 
+  
+
+  resetCoords() {
+    this.coords = null;
+    this.image = null;
+  }
+
+  makeBlobFromFile(file:any): Promise<Blob> {
+    return new Promise((resolve, reject) => {
+      (window as any).resolveLocalFileSystemURL(file, (fileEntry:any) => {
+        fileEntry.file((resFile:any) => {
+          const reader = new FileReader();
+          reader.onloadend = (evt: any) => {
+            const imgBlob: any = new Blob([evt.target.result], { type: 'image/jpeg' });
+            imgBlob.name = 'sample.jpg';
+            resolve(imgBlob);
+          };
+
+          reader.onerror = (e) => {
+            console.log('Failed file read: ' + e.toString());
+            reject(e);
+          };
+          reader.readAsArrayBuffer(resFile);
+        });
+      });
+    });
+  }
+
+  rotateImage(imageBlob:any): Promise<Blob> {
+    const urlCreator = window.URL || (window as any).webkitURL;
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+
+      img.onload = () => {
+        exif.getData(img, function () {
+          let width = img.width,
+              height = img.height,
+              canvas = document.createElement('canvas'),
+              ctx = canvas.getContext("2d"),
+              srcOrientation = exif.getTag(this, "Orientation");
+
+          if ([5,6,7,8].indexOf(srcOrientation) > -1) {
+            canvas.width = height;
+            canvas.height = width;
+          } else {
+            canvas.width = width;
+            canvas.height = height;
+          }
+
+          switch (srcOrientation) {
+            case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+            case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+            case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+            case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+            case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+            case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+            case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+            default: ctx.transform(1, 0, 0, 1, 0, 0);
+          }
+
+          ctx.drawImage(img, 0, 0);
+          canvas.toBlob(blob => resolve(blob))
+        });
+      }
+
+      img.src = urlCreator.createObjectURL(imageBlob);
+    });
+  }
 }
