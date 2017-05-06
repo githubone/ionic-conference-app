66c783b68dd5b251c8fd43fe12e659eced5bd715
Y Chan <ychan@ys-imac.home>
2017-05-06 19:02:03 +0100

    fix missing packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,6 @@ import { SupportPage } from '../pages/support/support';
 
 import { ConferenceData } from '../providers/conference-data';
 import { UserData } from '../providers/user-data';
-import { VideoPage } from '../pages/videos/video.page';
-import { WelcomePage} from '../pages/welcome/welcome';
-import { PhotoPage } from '../pages/photo/photo';
 
 export interface PageInterface {
   title: string;
@@ -45,19 +42,10 @@ export class ConferenceApp {
   // the left menu only works after login
   // the login page disables the left menu
   appPages: PageInterface[] = [
-<<<<<<< HEAD
-     { title: 'Photos', name: 'Photo', index: 5, component: PhotoPage, icon: 'information-circle' },
-     { title: 'Videos', name: 'VideoPage', index: 4, component: VideoPage, icon: 'information-circle' },
-    { title: 'Schedule', name: 'TabsPage', component: TabsPage, tabComponent: SchedulePage, index: 0, icon: 'calendar' },
-    { title: 'Speakers', name: 'TabsPage', component: TabsPage, tabComponent: SpeakerListPage, index: 1, icon: 'contacts' },
-    { title: 'Map', name: 'TabsPage', component: TabsPage, tabComponent: MapPage, index: 2, icon: 'map' },
-    { title: 'About', name: 'TabsPage', component: TabsPage, tabComponent: AboutPage, index: 3, icon: 'information-circle' }
-=======
     { title: 'Schedule', name: 'Tabs', component: TabsPage, tabComponent: SchedulePage, index: 0, icon: 'calendar' },
     { title: 'Speakers', name: 'Tabs', component: TabsPage, tabComponent: SpeakerListPage, index: 1, icon: 'contacts' },
     { title: 'Map', name: 'Tabs', component: TabsPage, tabComponent: MapPage, index: 2, icon: 'map' },
     { title: 'About', name: 'Tabs', component: TabsPage, tabComponent: AboutPage, index: 3, icon: 'information-circle' }
->>>>>>> 9a4762729a965eea56ed3f0eaeda80e4ffedb11a
   ];
   loggedInPages: PageInterface[] = [
     { title: 'Account', name: 'AccountPage', component: AccountPage, icon: 'person' },
