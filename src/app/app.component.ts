2c56c62429205e263e01596561d9c4c5cb25efc9
Y Chan <ychan@ys-imac.home>
2017-05-28 20:43:24 +0100

    modify to align with upstream master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,12 @@ import { SupportPage } from '../pages/support/support';
 import { ConferenceData } from '../providers/conference-data';
 import { UserData } from '../providers/user-data';
 
+
+import { VideoPage } from '../pages/videos/video.page';
+import { WelcomePage} from '../pages/welcome/welcome';
+import { PhotoPage } from '../pages/photo/photo';
+import { NetworkService} from '../providers/network-service';
+
 export interface PageInterface {
   title: string;
   name: string;
@@ -45,7 +51,9 @@ export class ConferenceApp {
     { title: 'Schedule', name: 'TabsPage', component: TabsPage, tabComponent: SchedulePage, index: 0, icon: 'calendar' },
     { title: 'Speakers', name: 'TabsPage', component: TabsPage, tabComponent: SpeakerListPage, index: 1, icon: 'contacts' },
     { title: 'Map', name: 'TabsPage', component: TabsPage, tabComponent: MapPage, index: 2, icon: 'map' },
-    { title: 'About', name: 'TabsPage', component: TabsPage, tabComponent: AboutPage, index: 3, icon: 'information-circle' }
+    { title: 'About', name: 'TabsPage', component: TabsPage, tabComponent: AboutPage, index: 3, icon: 'information-circle' },
+      { title: 'Photos', name: 'Photo', index: 5, component: PhotoPage, icon: 'information-circle' },
+      { title: 'Videos', name: 'VideoPage', index: 4, component: VideoPage, icon: 'information-circle' }
   ];
   loggedInPages: PageInterface[] = [
     { title: 'Account', name: 'AccountPage', component: AccountPage, icon: 'person' },
